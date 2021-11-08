c8e13a65e77631c55e038acf1fb3022c1a56f4c8
Morabito Federico <morabitofj@gmail.com>
2021-11-07 22:49:32 -0300

    BASDW8: paths to sessions section added

--- index.js
+++ index.js
@@ -9,4 +9,12 @@ app.listen(3000, () => {
   console.log('Server listening at port 3000');
 });
 
-app.get('/sessions', sessions.getAllSessions)
+// Sessions
+app.get('/sessions', sessions.getAllSessions);
+app.get('/sessions/searchById/:id', sessions.getSessionById);
+app.get('/sessions/add', sessions.createNewSession);
+app.get('/sessions/update/:id', sessions.editSessionById);
+app.get('/sessions/delete/:id', sessions.deleteSessionById);
+app.get('/sessions/searchByPostulant/:postulant_id', sessions.getSessionByPostulantId);
+app.get('/sessions/searchByCouncelor/:councelor_id', sessions.getSessionByCouncelorId);
+app.get('/sessions/searchByAccomplished/:accomplished', sessions.getSessionByAccomplishedState);
