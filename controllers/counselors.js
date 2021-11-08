55b55e77dde14c9a4720000664c98669d32cf6f1
Naila Ghigo <nailaghigo20@gmail.com>
2021-11-08 00:22:28 -0300

    BaSD: Week8 = Mindset Project = added error handling to counselors

--- controllers/counselors.js
+++ controllers/counselors.js
@@ -2,32 +2,36 @@ const counselors = require('../data/counselors.json');
 
 // Getting all Counselors
 const getCounselors = (req, res) => {
-    console.log('GET COUNSELORS');
+    if (counselors.length === 0){
+        return res.status(400).json({ msg: `Counselors not found` })
+    }
     return res.json(counselors);
 }
 
 // Getting Counselors by Country
 const getCounselorsByCountry = (req, res) => {
     const country = req.params.country;
-    console.log(country);
     let filterCounselors = counselors.filter(item => item.country.toLowerCase().includes(country.toLowerCase()))
-    console.log('GET COUNSELORS BY COUNTRY');
+    if (filterCounselors.length === 0){
+        return res.status(400).json({ msg: `Counselors from country ${country} not found` })
+    }
     return res.json(filterCounselors);
 }
 
 // Getting Counselor by ID
 const getCounselorsById = (req, res) => {
     const id = req.params.id;
-    console.log(id);
     let filterCounselorsId = counselors.filter(item => item.id == id)
-    console.log('GET COUNSELORS BY ID');
+    if (filterCounselorsId.length === 0){
+        return res.status(400).json({ msg: `Counselor with ID ${id} not found` })
+    }
     return res.json(filterCounselorsId);
 }
+
 
 
 module.exports = {
     getCounselors,
     getCounselorsByCountry,
     getCounselorsById,
-
 };
