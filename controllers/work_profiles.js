1dc17e83f3353f4b43f7ac5d9357c335bba0d12e
Bruno_Moyano <bruno_moyano@hotmail.com>
2021-11-09 02:05:55 -0300

    add feature delete row by id and overwrite json file

--- controllers/work_profiles.js
+++ controllers/work_profiles.js
@@ -1,9 +1,13 @@
-//Get all work profiles
+const fs = require('fs');
+
+//This function returns all work profiles
 
 const getAllWP = (req, res) => {
   const response = require('../data/work_profles.json');
   res.send(response);
 };
+
+//This function returns all positions by name
 
 const getWPByName = (req, res) => {
   const response = require('../data/work_profles.json');
@@ -11,14 +15,27 @@ const getWPByName = (req, res) => {
   res.send(nameWP);
 };
 
+//This function returns all positions by id
+
 const getWPByID = (req,res) => {
   const response = require('../data/work_profles.json');
   let idWP = response.filter(profile => profile.id == req.params.position_id);
   res.send(idWP);
 };
 
+//This function deletes one row by id and overwrites json file
+
+const deleteUpdateWP = (req,res) =>{
+  const filepath = './data/work_profles.json';
+  const fileObject = JSON.parse(fs.readFileSync(filepath, 'utf8'));
+  const deleteWP = fileObject.filter(profile => profile.id != req.params.id);
+  fs.writeFileSync(filepath,JSON.stringify(deleteWP,null,2),'utf8');
+  res.send(deleteWP);
+};
+
 module.exports= {
   getAllWP,
   getWPByName,
-  getWPByID
+  getWPByID,
+  deleteUpdateWP
 };
--- routes/positions.js
+++ routes/positions.js
@@ -1,10 +1,11 @@
 const {Router} = require('express');
-const { getAllWP, getWPByName, getWPByID } = require('../controllers/work_profiles');
+const { getAllWP, getWPByName, getWPByID, deleteUpdateWP } = require('../controllers/work_profiles');
 const positionRoutes = Router();
 
 positionRoutes.get('/',getAllWP);
 positionRoutes.get('/name/:position_name',getWPByName);
 positionRoutes.get('/id/:position_id',getWPByID);
+positionRoutes.get('/delete/:id',deleteUpdateWP);
 
 module.exports= {
     positionRoutes
