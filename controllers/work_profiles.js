ec071cfbecccc72207caed2a750868d4ed2c0551
Bruno_Moyano <bruno_moyano@hotmail.com>
2021-11-09 02:08:17 -0300

    fix name work_profiles in all sites where use it

--- controllers/work_profiles.js
+++ controllers/work_profiles.js
@@ -3,14 +3,14 @@ const fs = require('fs');
 //This function returns all work profiles
 
 const getAllWP = (req, res) => {
-  const response = require('../data/work_profles.json');
+  const response = require('../data/work_profiles.json');
   res.send(response);
 };
 
 //This function returns all positions by name
 
 const getWPByName = (req, res) => {
-  const response = require('../data/work_profles.json');
+  const response = require('../data/work_profiles.json');
   let nameWP = response.filter(profile => profile.position_name == req.params.position_name);
   res.send(nameWP);
 };
@@ -18,7 +18,7 @@ const getWPByName = (req, res) => {
 //This function returns all positions by id
 
 const getWPByID = (req,res) => {
-  const response = require('../data/work_profles.json');
+  const response = require('../data/work_profiles.json');
   let idWP = response.filter(profile => profile.id == req.params.position_id);
   res.send(idWP);
 };
@@ -26,7 +26,7 @@ const getWPByID = (req,res) => {
 //This function deletes one row by id and overwrites json file
 
 const deleteUpdateWP = (req,res) =>{
-  const filepath = './data/work_profles.json';
+  const filepath = './data/work_profiles.json';
   const fileObject = JSON.parse(fs.readFileSync(filepath, 'utf8'));
   const deleteWP = fileObject.filter(profile => profile.id != req.params.id);
   fs.writeFileSync(filepath,JSON.stringify(deleteWP,null,2),'utf8');
