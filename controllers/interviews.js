7a4f1158a71af52e256c52fbb11888f1a406c798
Micaela Casais <casaismicaela@gmail.com>
2021-11-11 00:23:34 -0300

    MS: corrections made as per requested(comments removed, one function changed to arrow function, messages returned as a json format swapped to a string

--- controllers/interviews.js
+++ controllers/interviews.js
@@ -1,25 +1,22 @@
 const fs = require('fs');
-const { stringify } = require('querystring');
 const interviews =  require('../data/interviews.json');
 
 
-//Getting all interviews
 const getInterviews = (req, res) => {
     return res.json(interviews);
 };
-//Getting a single interview by Job Title
+
 const getInterviewsByJobTitle= (req, res) => {
     let filtInterviews= interviews.filter(item => item.job_title.includes(req.params.job_title));
     return res.json(filtInterviews);
 };
-//Getting a single interview by Company
 
 const getInterviewsByCompany= (req, res) => {
     let filtInterviews2= interviews.filter(item => item.company_name === req.params.company_name);
     return res.json(filtInterviews2);
 };
-//Creating an interview
-function createInterview(req, res) {
+
+const createInterview =(req, res) => {
     const newInterview = {
         id: (interviews.length) + 1,
         job_title: req.query.job_title,
@@ -28,18 +25,17 @@ function createInterview(req, res) {
         time: req.query.time,
     };
     if (!newInterview.job_title || !newInterview.company_name || !newInterview.date || !newInterview.time) {
-        return res.status(400).json({ msg: 'Please include a job title, company name, date and time' });
+        return res.status(400).send('Please include a job title, company name, date and time');
     }
     interviews.push(newInterview);
     fs.writeFile('./data/interviews.json', JSON.stringify(interviews), err => {
         if (err) {
-            res.status(500).json({ msg: `Error while saving data` });
+            res.status(500).send(`Error while saving data`);
         }
     });
-    res.json(interviews);
+    res.status(201).json(newInterview);
 }
 
-//Updating an interview
 const updateInterview = (req, res) => {
     let itemPosition = -1;
     let filtInterviews3= interviews.find((item,index) => {
@@ -59,13 +55,13 @@ const updateInterview = (req, res) => {
         res.status(400).json({ msg: ` error . no interview with the id of ${req.params.id} ` });
     }
         fs.writeFile('./data/interviews.json',JSON.stringify(interviews),err => {
-            if (err) { res.status(500).json({ msg: `Error while saving interviews with updated item` });
+            if (err) { 
+                res.status(500).send(`Error while saving interviews with updated item`);
             } else {
             res.json({ msg: 'Interview updated', filtInterviews3});
             }})
 };
 
-//Deleting an interview
 const deleteInterview = (req, res) => {
     let itemPosition = -1;
     let filtInterviews4= interviews.find((item,index) => {
@@ -76,7 +72,8 @@ const deleteInterview = (req, res) => {
     if (filtInterviews4) {
         interviews.splice(itemPosition, 1);
         fs.writeFile('./data/interviews.json',JSON.stringify(interviews),err => {
-            if (err) { res.status(500).json({ msg: `Error while saving interviews with deleted item` });
+            if (err) {
+                res.status(500).send(`Error while saving interviews with deleted item`);
             }})
         res.json({ msg: `Interview with id of ${req.params.id} deleted`});
         } else {
--- controllers/positions.js
+++ controllers/positions.js
@@ -1,43 +1,42 @@
 const fs = require('fs');
 const positions =  require('../data/positions.json');
 
-//Getting all job positions
+
 const getAllPositions = (req, res) => {
     return res.json(positions)
 };
-//Getting a single job position
+
 const getPositionsByJobTitle= (req, res) => {
     let filtPositions= positions.filter(item => item.job_title.includes(req.params.job_title));
     return res.json(filtPositions);
 };
-//Creating a job position
+
 const createJobPosition = (req, res) => {
-    const newPosition = {
-        id: (positions.length)+1,
-        job_title: req.query.job_title,
-        company_id: req.query.company_id,
-        company_name: req.query.company_name,
-        job_descriptions: req.query.job_descriptions,
-        city: req.query.city,
-        country: req.query.country,
-        date_posted: req.query.date_posted,
-        closing_date: req.query.closing_date,
-    }
-    if (!newPosition.job_title || !newPosition.company_id || !newPosition.company_name || !newPosition.job_descriptions || 
-        !newPosition.city || !newPosition.country || !newPosition.date_posted || !newPosition.closing_date) {
-        return res.status(400).json({ msg: 'Please include all fields to create a new Job Position', });
+    if (!req.query.job_title || !req.query.company_id || !req.query.company_name || !req.query.job_descriptions || 
+        !req.query.city || !req.query.country || !req.query.date_posted || !req.query.closing_date) {
+            return res.status(400).send('Please include all fields to create a new Job Position');
     } else {
+        const newPosition = {
+            id: positions.length + 1,
+            job_title: req.query.job_title,
+            company_id: req.query.company_id,
+            company_name: req.query.company_name,
+            job_descriptions: req.query.job_descriptions,
+            city: req.query.city,
+            country: req.query.country,
+            date_posted: req.query.date_posted,
+            closing_date: req.query.closing_date,
+        }
         positions.push(newPosition);
-        res.status(200).json({ msg: `Position with the id of ${newPosition.id} created` });
+        fs.writeFile('./data/positions.json', JSON.stringify(positions), err => {
+            if (err) {
+                res.status(500).send(`Error while saving created position`);
+            } else {
+                res.status(201).json({ msg: 'Position created', newPosition});
+            }})
     }
-    fs.writeFile('./data/positions.json', JSON.stringify(positions), err => {
-        if (err) {
-            res.status(500).json({ msg: `Error while saving created position` });
-        } else {
-            res.status(200).json({ msg: 'Position created', newPosition});
-        }})
 };
-//Updating a position
+
 const updatePosition = (req, res) => {
     let itemPosition = -1;
     let filtPositions3= positions.find((item,index) => {
@@ -62,13 +61,13 @@ const updatePosition = (req, res) => {
             res.status(400).json({ msg: ` No position with the id of ${req.params.id} to update` });
         }
         fs.writeFile('./data/positions.json',JSON.stringify(positions),err => {
-            if (err) { res.status(500).json({ msg: `Error while saving positions with updated item` });
+            if (err) { 
+                res.status(500).send(`Error while saving positions with updated item`);
             } else {
             res.json({ msg: 'Position updated', filtPositions3});
             }})
 };
 
-//Deleting an interview
 const deletePosition = (req, res) => {
     let itemPosition = -1;
     let filtPositions4= positions.find((item,index) => {
@@ -79,7 +78,8 @@ const deletePosition = (req, res) => {
     if (filtPositions4) {
         positions.splice(itemPosition, 1);
         fs.writeFile('./data/positions.json',JSON.stringify(positions),err => {
-            if (err) { res.status(500).json({ msg: `Error while saving positions with deleted item` });
+            if (err) { 
+                res.status(500).send(`Error while saving positions with deleted item`);
             }})
         res.json({ msg: `position with id of ${req.params.id} deleted`});
         } else {
